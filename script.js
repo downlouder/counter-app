ca8744393fb09270cfd7a43dbd14516a655e112c
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-12 01:43:44 +0200

    New mechanic

--- script.js
+++ script.js
@@ -3,28 +3,19 @@ const decrement = document.getElementById('decrement');
 const reset = document.getElementById('reset');
 const increment = document.getElementById('increment');
 let value = 0;
+let colorValue = 56;
 
 function changeColor() {
     if (value === 0) {
-        counter.style.color = 'yellow';
+        colorValue = 56;
+        counter.style.color = `hsl(${colorValue}, 100%, 50%)`;
     }
-    else if (value > 10 && value <= 25) {
-        counter.style.color = 'lightgreen';
+    else if (value > 0) {
+        colorValue++;
+        counter.style.color = `hsl(${++colorValue}, 100%, 50%)`;
     }
-    else if (value > 25 && value <= 50) {
-        counter.style.color = 'green';
-    }
-    else if (value > 50 && value < 100) {
-        counter.style.color = 'lightblue';
-    }
-    else if (value < 0 && value >= -10) {
-        counter.style.color = 'orange';
-    }
-    else if (value < -10 && value >= -25) {
-        counter.style.color = 'orangered';
-    }
-    else if (value < -25 && value >= -50) {
-        counter.style.color = 'red';
+    else if (value < 0) {
+        counter.style.color = `hsl(${--colorValue}, 100%, 50%)`;
     }
 }
 

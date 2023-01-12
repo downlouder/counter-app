a52786a30fee7b405a17454b22c4014cba936627
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-12 02:56:01 +0200

    Fix bugs and some styles

--- script.js
+++ script.js
@@ -2,40 +2,61 @@ const counter = document.getElementById('counter');
 const decrement = document.getElementById('decrement');
 const reset = document.getElementById('reset');
 const increment = document.getElementById('increment');
+const label = document.getElementById('label');
 let value = 0;
 let colorValue = 56;
 
-function changeColor() {
-    if (value === 0) {
-        colorValue = 56;
-        counter.style.color = `hsl(${colorValue}, 100%, 50%)`;
-    }
-    else if (value > 0) {
-        colorValue++;
-        counter.style.color = `hsl(${++colorValue}, 100%, 50%)`;
-    }
-    else if (value < 0) {
-        counter.style.color = `hsl(${--colorValue}, 100%, 50%)`;
-    }
-}
-
+// function changeColor() {
+//     if (value === 0) {
+//         colorValue = 56;
+//         label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+//         counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+//     }
+//     else if (value > 0) {
+//         ++colorValue;
+//         if (colorValue % 2 === 0) {
+//             label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+//             counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+//         }
+//     }
+//     else if (value < 0) {
+//         --colorValue;
+//         if (colorValue % 2 === 0) {
+//             label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+//             counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+//         }
+//     }
+// }
 
 function app() {
     counter.innerHTML = value;
-    changeColor();
+    label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+    counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
     increment.addEventListener('click', () => {
         value++;
-        changeColor();
+        ++colorValue;
+        if (colorValue % 2 === 0) {
+            label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+            counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+        }
         counter.innerHTML = value;
     });
     decrement.addEventListener('click', () => {
         value--;
-        changeColor();
+        --colorValue;
+        if (colorValue % 2 === 0) {
+            label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+            counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+        }
         counter.innerHTML = value;
     });
     reset.addEventListener('click', () => {
         value = 0
-        changeColor();
+        if (value === 0) {
+            colorValue = 56;
+            label.style.color = `hsl(${colorValue}, 100%, 85%)`;
+            counter.style.color = `hsl(${colorValue}, 100%, 55%)`;
+        }
         counter.innerHTML = value;
     });
 }
